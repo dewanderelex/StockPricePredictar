cb49e0fcd786e46eed7dec0946656a8640f3b1b0
dewanderelex <nguyvi01@gettysburg.edu>
2019-01-04 12:19:56 +0700

    test4

--- server.py
+++ server.py
@@ -1,13 +1,14 @@
 # webapp
 from flask import Flask, jsonify, render_template, request
 
-app = Flask(__name__, static_folder="../client/build", static_url_path='')
+#app = Flask(__name__, static_folder="../client/build", static_url_path='')
+app = Flask(__name__)
 
 @app.route('/')
 def index():
     #return app.send_static_file('index.html')
     return "<h1> Deployed </h1>"
 
-if __name__ == '__main__':
-    #app.run(debug=True, host='0.0.0.0', port = 8080)
-    app.run(debug = True)
+# if __name__ == '__main__':
+#     #app.run(debug=True, host='0.0.0.0', port = 8080)
+#     app.run(debug = True)
